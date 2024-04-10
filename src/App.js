a9aade58f6ba6332d456a1f7576dcff51a07b874
Hansikarn <Hansika.karn@gmail.com>
2024-04-11 00:19:59 +0530

    added new component ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
       <h1>Let's get Started</h1>
+      <ExpenseItem></ExpenseItem>
       <p>I am learning react</p>
     </div>
   );
