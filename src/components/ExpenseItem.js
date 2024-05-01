66d0c6a77fb9e667d03692c2c1883103273c35ce
Hansikarn <Hansika.karn@gmail.com>
2024-05-01 18:57:22 +0530

    Styling components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,15 @@
-function ExpenseItem(){
-    return <div>
-    <p>Food - $5</p>
+import './ExpenseItem.css';
 
-   <p> Petrol - $20</p>
-    
-   <p> Movie - $10</p></div>
-};
+function ExpenseItem(){
+    return(
+    <div className='expense-item'>
+        <div>15 August 2001</div>
+        <div>Delhi</div>
+        <div className='expense-item__description'>
+            <h2>book</h2>
+            <div className='expense-item__price'>$10</div>
+        </div>
+    </div>
+    );
+}
 export default ExpenseItem;
