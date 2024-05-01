84c00cebbb42bea5cb9d71cff8d893db41f7bd7b
Hansikarn <Hansika.karn@gmail.com>
2024-05-01 19:25:53 +0530

    javascript inside JSX
    used variables and javascript inside jsx.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(){
+    const expenseDate =new Date(2024,5,1).toISOString();
+    const expenseTitle = "book";
+    const expenseAmount=10;
+    const expenseLocation='Bangalore';
     return(
     <div className='expense-item'>
-        <div>15 August 2001</div>
-        <div>Delhi</div>
+        <div>{expenseDate}</div>
+        <div>{expenseLocation}</div>
         <div className='expense-item__description'>
-            <h2>book</h2>
-            <div className='expense-item__price'>$10</div>
+            <h2>{expenseTitle}</h2>
+            <div className='expense-item__price'>${expenseAmount}</div>
         </div>
     </div>
     );
